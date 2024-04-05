ce09181f55b27de9ab7cc99d66f2a738494b7306
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-04-05 14:12:33 +0530

    get request

---
+++ Server/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+// port = 3000;
+
+const app = express();
+//define a route
+console.log('hello')
+app.get('/',(req,res)=>{
+    try{
+        res.send("Welcome to Joyce's capstone");
+    }
+    catch(err){
+        res.send(err)
+    }
+})
+
+//start server
+app.listen(process.env.PUBLIC_PORT,()=>{
+    console.log(`Server is running on port ${process.env.PUBLIC_PORT}`);
+})
