efbc4c7997b8e327957e2b2dc3387e8e12a24a65
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-04-26 16:53:54 +0530

    schema done

--- Server/server.js
+++ Server/server.js
@@ -13,21 +13,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Welcome to Joyce's capstone");
   });
-  
-  
-  app.get('/getHome', async(req, res) => {
-      try {
-          let data = await HomeModel.find()
-          console.log(data)
-          res.send(data)
-        } catch (error) {
-            res.send(error) 
-        }
-        
-    });
     
     connectToDB();
-    
+
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
