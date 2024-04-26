6355ffdf8d041acb25f51d9a478a3d96870e9f3e
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-04-26 14:03:34 +0530

    connected to database

---
+++ Server/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const uri = process.env.mongoURI;
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+
+    process.exit(1);
+  }
+};
+
+module.exports = connectToDB;
