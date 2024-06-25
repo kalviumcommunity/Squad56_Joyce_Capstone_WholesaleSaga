95fb156921ca27a4e5faabf492b042ec0001bc77
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-06-25 17:48:41 +0530

    saga changed to sagas

--- Server/models/user.js
+++ Server/models/user.js
@@ -3,6 +3,6 @@ const HomeSchema = new mongoose.Schema({
     img:String,
     category:String
 })
-const HomeModel =mongoose.model("whole_saga",HomeSchema)
+const HomeModel =mongoose.model("whole_sagas",HomeSchema)
 module.exports = HomeModel
 // I am raising a pr for my "Database schema created" which is a redo assingment as The added lines in the code did not include the implementation of creating Mongoose models along with schema.
