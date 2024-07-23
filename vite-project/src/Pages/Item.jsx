9b7485d069546b3ec5616d723ce09d1398c7520e
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-07-23 13:53:05 +0530

    display items

--- vite-project/src/Pages/Item.jsx
+++ vite-project/src/Pages/Item.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer"
@@ -8,10 +8,72 @@ import plus from "../assets/plus.png"
 
 
 function Item(){
+
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+      axios.get('https://squad56-joyce-capstone-wholesalesaga-1.onrender.com/getCat')
+        .then(response => {
+          setItem(response.data);
+        })
+        .catch(error => {
+          console.log("Error", error);
+        });
+    }, []);
+
+
+
     return(
         <>
         <Navbar/>
         <div className="catName">CHICKEN</div>
+
+        <div className="catContainer">
+            {item.map(user=>(
+                <div className="catItem">
+                    <img className="catImage" src={user.img} alt="!" />
+                    <font className="catFname">{user.name}</font>
+                    <font className="catWeight">{user.weight}</font>
+                    <font className="catPieces">{user.serve}</font>
+                    <font className="catPrice">{user.price}</font>
+                </div>
+            ))}
+        </div>
+
+
+
+        
+
+        {/* <div className="catContainer">
+            <div className="catItem">
+            <img className="catImage" src={liver} alt="!" />
+            <font className="catFname">Chicken Liver</font>
+            <font className="catWeight">500g</font>
+            <font className="catPieces">| 20-30 Pieces | Serves 4</font>
+            <font className="catPrice">₹379</font>
+            <button className="Badd"><img  className="add" src={plus} alt="!"/></button>
+            </div>
+            <div className="catItem">
+            <img className="catImage" src={liver} alt="!" />
+            <font className="catFname">Chicken Curry Cut - Small Pieces</font>
+            <font className="catWeight">500g</font>
+            <font className="catPieces">| 20-30 Pieces | Serves 4</font>
+            <font className="catPrice">₹379</font>
+            <button className="Badd"><img  className="add" src={plus} alt="!"/></button>
+            </div>
+            <div className="catItem">
+            <img className="catImage" src={liver} alt="!" />
+            <font className="catFname">Chicken Breast - Boneless</font>
+            <font className="catWeight">500g</font>
+            <font className="catPieces">| 20-30 Pieces | Serves 4</font>
+            <font className="catPrice">₹379</font>
+            <button className="Badd"><img  className="add" src={plus} alt="!"/></button>
+            </div>
+        </div> */}
+
+
+
+{/*         
         <div className="catContainer">
             <div className="catItem">
             <img className="catImage" src={liver} alt="!" />
@@ -37,8 +99,12 @@ function Item(){
             <font className="catPrice">₹379</font>
             <button className="Badd"><img  className="add" src={plus} alt="!"/></button>
             </div>
-        </div>
-        <div className="catContainer">
+        </div> */}
+
+
+
+
+        {/* <div className="catContainer">
             <div className="catItem">
             <img className="catImage" src={liver} alt="!" />
             <font className="catFname">Chicken Liver</font>
@@ -63,32 +129,7 @@ function Item(){
             <font className="catPrice">₹379</font>
             <button className="Badd"><img  className="add" src={plus} alt="!"/></button>
             </div>
-        </div><div className="catContainer">
-            <div className="catItem">
-            <img className="catImage" src={liver} alt="!" />
-            <font className="catFname">Chicken Liver</font>
-            <font className="catWeight">500g</font>
-            <font className="catPieces">| 20-30 Pieces | Serves 4</font>
-            <font className="catPrice">₹379</font>
-            <button className="Badd"><img  className="add" src={plus} alt="!"/></button>
-            </div>
-            <div className="catItem">
-            <img className="catImage" src={liver} alt="!" />
-            <font className="catFname">Chicken Curry Cut - Small Pieces</font>
-            <font className="catWeight">500g</font>
-            <font className="catPieces">| 20-30 Pieces | Serves 4</font>
-            <font className="catPrice">₹379</font>
-            <button className="Badd"><img  className="add" src={plus} alt="!"/></button>
-            </div>
-            <div className="catItem">
-            <img className="catImage" src={liver} alt="!" />
-            <font className="catFname">Chicken Breast - Boneless</font>
-            <font className="catWeight">500g</font>
-            <font className="catPieces">| 20-30 Pieces | Serves 4</font>
-            <font className="catPrice">₹379</font>
-            <button className="Badd"><img  className="add" src={plus} alt="!"/></button>
-            </div>
-        </div>
+        </div> */}
         <Footer/>
         </>
     )
