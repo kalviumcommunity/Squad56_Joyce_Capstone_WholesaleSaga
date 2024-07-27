6d978e4ac83a64591d5d7050252e6a3b5490aeb5
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-07-27 18:35:31 +0530

    display done error2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -29,11 +29,11 @@ function App() {
         <Route path='/Seafood' element={<Seafood/>}></Route>
         <Route path='/Ready' element={<Ready/>}></Route>
         <Route path='/Snack' element={<Snack/>}></Route>
-        <Route path='"/Cold' element={<Cold/>}></Route>
-        <Route path='"/Egg' element={<Egg/>}></Route>
-        <Route path='"/Pickel' element={<Pickel/>}></Route>
-        <Route path='"/Spice' element={<Spice/>}></Route>
-        <Route path='"/Dryfish' element={<Dryfish/>}></Route>
+        <Route path='/Cold' element={<Cold/>}></Route>
+        <Route path='/Egg' element={<Egg/>}></Route>
+        <Route path='/Pickel' element={<Pickel/>}></Route>
+        <Route path='/Spice' element={<Spice/>}></Route>
+        <Route path='/Dryfish' element={<Dryfish/>}></Route>
       </Routes>
      </BrowserRouter>
     </>
